ee0a221c43089426e375ff291b3524646126ae44
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-08-30 12:52:07 +0600

    service class refactored

---
+++ src/main/java/com/blogapi/controllers/UserController.java
@@ -0,0 +1,46 @@
+package com.blogapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blogapi.services.UserServices;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserServices userServices;
+
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return userServices.getAllUsers();
+    }
+
+    @RequestMapping("users/{id}")
+    public User getUser(@PathVariable int id) {
+        return userServices.getUser(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public void addUser(@RequestBody User user) {
+        userServices.addUser(user);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/users/{id}")
+    public void updateUser(@RequestBody User user, @PathVariable int id) {
+        userServices.updateUser(user, id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userServices.deleteUser(id);
+    }
+
+}
---
+++ src/main/java/com/blogapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.blogapi.repository;
+
+import com.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
